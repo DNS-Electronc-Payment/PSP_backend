b9c056d5cba4dd3843771bc7683c23dcde1354d1
Ninoli1 <nikolinaskiljevic@gmail.com>
2024-11-20 22:54:29 +0100

    Initial Setup

---
+++ PSP_backend/src/main/java/com/project/PSP_backend/PspBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.PSP_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PspBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PspBackendApplication.class, args);
+	}
+
+}
